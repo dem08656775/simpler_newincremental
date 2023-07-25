ce47a8909e48a819d0324c5df2dddcc121892a7c
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2023-07-25 23:37:09 +0900

    Add files via upload

---
+++ game.js
@@ -0,0 +1,526 @@
+const initialData = () => {
+  return {
+    money: new Decimal(1),
+    level: new Decimal(0),
+    levelresettime: new Decimal(0),
+    maxlevelgained: new Decimal(1),
+    token: 0,
+
+    rank:new Decimal(0),
+    rankresettime: new Decimal(0),
+
+    generators: new Array(8).fill(null).map(() => new Decimal(0)),
+    generatorsBought: new Array(8).fill(null).map(() => new Decimal(0)),
+    generatorsCost: [
+      new Decimal(1),
+      new Decimal('1e2'),
+      new Decimal('1e6'),
+      new Decimal('1e12'),
+      new Decimal('1e20'),
+      new Decimal('1e30'),
+      new Decimal('1e42'),
+      new Decimal('1e56')
+    ],
+    generatorsMode: new Array(8).fill(null).map((_, i) => i),
+
+    accelerators: new Array(8).fill(null).map(() => new Decimal(0)),
+    acceleratorsBought: new Array(8).fill(null).map(() => new Decimal(0)),
+    acceleratorsCost: [
+      new Decimal(10),
+      new Decimal('1e10'),
+      new Decimal('1e20'),
+      new Decimal('1e40'),
+      new Decimal('1e80'),
+      new Decimal('1e160'),
+      new Decimal('1e320'),
+      new Decimal('1e640'),
+    ],
+
+    tickspeed: 100,
+
+    currenttab: 'basic',
+    tweeting:['money'],
+    onchallenge:false,
+    challenges:[],
+    challengecleared:[],
+    challengebonuses:[],
+
+    levelitems:[0,0]
+
+  }
+}
+
+Vue.createApp({
+  data() {
+    return {
+      player: {
+        money: new Decimal(1),
+        level: new Decimal(0),
+        levelresettime: new Decimal(0),
+        maxlevelgained: new Decimal(1),
+        token:0,
+
+        rank:new Decimal(0),
+        rankresettime: new Decimal(0),
+
+        generators: new Array(8).fill(null).map(() => new Decimal(0)),
+        generatorsBought: new Array(8).fill(null).map(() => new Decimal(0)),
+        generatorsCost: [
+          new Decimal(1),
+          new Decimal('1e4'),
+          new Decimal('1e9'),
+          new Decimal('1e16'),
+          new Decimal('1e25'),
+          new Decimal('1e36'),
+          new Decimal('1e49'),
+          new Decimal('1e64')
+        ],
+        generatorsMode: new Array(8).fill(null).map((_, i) => i),
+
+        accelerators: new Array(8).fill(null).map(() => new Decimal(0)),
+        acceleratorsBought: new Array(8).fill(null).map(() => new Decimal(0)),
+        acceleratorsCost: [
+          new Decimal(10),
+          new Decimal('1e10'),
+          new Decimal('1e20'),
+          new Decimal('1e40'),
+          new Decimal('1e80'),
+          new Decimal('1e160'),
+          new Decimal('1e320'),
+          new Decimal('1e640'),
+        ],
+
+        tickspeed: 100,
+
+        currenttab: 'basic',
+
+        tweeting:['money'],
+
+        onchallenge: false,
+        challenges: [],
+        challengecleared: [],
+        challengebonuses:[],
+
+        levelitems:[0,0],
+      },
+      challengedata: new Challengedata(),
+      levelshopdata: new Levelshopdata(),
+      exported: "",
+      activechallengebonuses:[],
+      genautobuy:false,
+      accautobuy:false,
+      autolevel:false,
+
+
+    }
+  },
+
+  methods: {
+
+    softCap(num,cap){
+      if(num.lessThanOrEqualTo(cap)) return num;
+      let capped = num.div(cap)
+      capped = new Decimal(capped.log2()).add(1)
+      return cap.mul(capped).min(num)
+    },
+
+    calcgncost(){
+      for(let i=0;i<8;i++){
+        let p = i === 0 ? this.player.generatorsBought[0] : this.player.generatorsBought[i].add(i).mul(i + 1)
+        if(this.player.onchallenge && this.player.challenges.includes(1) && this.player.generatorsBought[i].gt(0)){
+          p = p.mul(2)
+        }
+        this.player.generatorsCost[i] = new Decimal(10).pow(p)
+      }
+    },
+
+    calcaccost(){
+      for(let i=0;i<8;i++){
+        let p = this.player.acceleratorsBought[i].add(1)
+        p = p.mul(p.add(1)).div(2)
+        p = p.mul(i === 0 ? 1:new Decimal(10).mul(new Decimal(2).pow(i-1)))
+        this.player.acceleratorsCost[i] = p.pow_base(10)
+      }
+    },
+
+    calcincrementmult(i,to){
+      let mult = new Decimal(1);
+      if(!(this.player.onchallenge && this.player.challenges.includes(4))){
+        mult = mult.mul(new Decimal(10).pow((i + 1) * (i - to)))
+      }
+      if(!(this.player.onchallenge && this.player.challenges.includes(7))){
+        let cap = new Decimal(100).mul(new Decimal(2).pow(this.player.levelitems[0]))
+        mult = mult.mul(this.softCap(this.player.levelresettime.add(1),cap))
+      }
+      mult = mult.mul(new Decimal(this.player.level.add(2).log2()).pow(i - to))
+      let highest = 0;
+      for(let j=0;j<8;j++){
+        if(this.player.generators[j].greaterThan(0)){
+          highest = j;
+        }
+      }
+
+      if(!(this.player.onchallenge && this.player.challenges.includes(2))){
+        let mm = this.player.generatorsBought[i]
+        if(this.activechallengebonuses.includes(11)){
+          mm = mm.mul(new Decimal(mm.add(2).log2()).round())
+        }
+        if(i<highest && this.player.generatorsBought[i].greaterThan(0)){
+          mult = mult.mul(mm)
+        }else{
+          if(this.activechallengebonuses.includes(2) && this.player.generatorsBought[i].greaterThan(0)){
+            mult = mult.mul(mm)
+          }
+        }
+      }
+
+      if(this.activechallengebonuses.includes(3)){
+        mult = mult.mul(new Decimal(2))
+      }
+
+      if(i==0&&this.activechallengebonuses.includes(7)){
+        mult = mult.mul(this.player.maxlevelgained)
+      }
+
+
+      return mult;
+
+    },
+    update() {
+      this.activechallengebonuses = (this.player.challengebonuses.includes(4) || !this.player.onchallenge)?this.player.challengebonuses:[]
+      for (let i = 0; i < 8; i++) {
+        if(!this.activechallengebonuses.includes(13)){
+          let to = this.player.generatorsMode[i];
+          let mult = this.calcincrementmult(i,to)
+          if (to === 0) {
+            this.player.money = this.player.money.add(this.player.generators[i].mul(mult))
+          } else {
+            this.player.generators[to - 1] = this.player.generators[to - 1].add(this.player.generators[i].mul(mult))
+          }
+        }else{
+          if(this.player.onchallenge&&this.player.challenges.includes(3)){
+            let mult = this.calcincrementmult(i,0)
+            mult = mult.mul(i+1)
+            this.player.money = this.player.money.add(this.player.generators[i].mul(mult))
+          }else{
+            for(let to = 0; to <= i; to++){
+            let mult = this.calcincrementmult(i,to)
+              if (to === 0) {
+                this.player.money = this.player.money.add(this.player.generators[i].mul(mult))
+              } else {
+                this.player.generators[to - 1] = this.player.generators[to - 1].add(this.player.generators[i].mul(mult))
+              }
+            }
+          }
+        }
+      }
+      let amult = new Decimal(1)
+      if(this.activechallengebonuses.includes(6))amult = amult.mul(this.player.acceleratorsBought[0].max(1))
+
+      for (let i = 1; i < 8; i++) {
+        if(this.activechallengebonuses.includes(10)){
+          this.player.accelerators[i - 1] = this.player.accelerators[i - 1].add(this.player.accelerators[i].mul(this.player.acceleratorsBought[i]))
+        }else{
+          this.player.accelerators[i - 1] = this.player.accelerators[i - 1].add(this.player.accelerators[i])
+        }
+      }
+      if(!this.player.onchallenge && this.activechallengebonuses.includes(14) && this.autolevel){
+        if(this.player.money.greaterThanOrEqualTo('1e21')){
+          this.resetLevel(true,false)
+        }
+      }
+      if(this.activechallengebonuses.includes(9)&&this.accautobuy){
+        let ha = this.player.levelitems[1] + 1
+        for(let i=ha;i>=0;i--){
+          this.buyAccelerator(i)
+        }
+      }
+      if(this.activechallengebonuses.includes(5)&&this.genautobuy){
+        for(let i=7;i>=0;i--){
+          this.buyGenerator(i)
+        }
+      }
+
+      this.player.tickspeed = 100 / this.player.accelerators[0].add(10).mul(amult).log10()
+
+      setTimeout(this.update, this.player.tickspeed);
+    },
+    exportsave(){
+      this.exported = btoa(JSON.stringify(this.player))
+    },
+    importsave(){
+      let input = window.prompt("データを入力","")
+      input = atob(input)
+      localStorage.setItem("playerStoredoldold",input)
+      this.load()
+    },
+    save() {
+      console.log(JSON.stringify(this.player))
+      console.log(JSON.stringify(this.activechallengebonuses))
+      localStorage.setItem("playerStoredold", JSON.stringify(this.player));
+    },
+    load() {
+      if (!localStorage.getItem("playerStoredold")) return
+      let saveData = JSON.parse(localStorage.getItem("playerStoredold"));
+
+      this.player = {
+          money: new Decimal(saveData.money),
+          level: new Decimal(saveData.level),
+          levelresettime: new Decimal(saveData.levelresettime),
+          maxlevelgained: new Decimal(saveData.maxlevelgained ?? 1),
+          token: saveData.token ?? 0,
+
+          rank: new Decimal(saveData.rank ?? 0),
+          rankresettime: new Decimal(saveData.rankresettime ?? 0),
+
+          generators: saveData.generators.map(v => new Decimal(v)),
+          generatorsBought: saveData.generatorsBought.map(v => new Decimal(v)),
+          generatorsCost: saveData.generatorsCost.map(v => new Decimal(v)),
+          generatorsMode: saveData.generatorsMode.map(v => parseInt(v)),
+
+          accelerators: saveData.accelerators.map(v => new Decimal(v)),
+          acceleratorsBought: saveData.acceleratorsBought.map(v => new Decimal(v)),
+          acceleratorsCost: saveData.acceleratorsCost.map(v => new Decimal(v)),
+
+          tickspeed: parseFloat(saveData.tickspeed),
+          saveversion: parseInt(saveData.saveversion),
+
+          currenttab: saveData.currenttab ?? 'basic',
+
+          tweeting: saveData.tweeting ?? ['money'],
+
+          onchallenge: saveData.onchallenge ?? false,
+          challenges: saveData.challenges ?? [],
+          challengecleared: saveData.challengecleared ?? [],
+          challengebonuses: saveData.challengebonuses ?? [],
+
+          levelitems: saveData.levelitems ?? [0,0],
+
+      }
+      if(!this.player.onchallenge || this.player.challengebonuses.includes(4))this.activechallengebonuses = this.player.challengebonuses
+      this.calcgncost()
+      this.calcaccost()
+    },
+    changeTab(tabname){
+      this.player.currenttab = tabname;
+    },
+    configchallenge(index){
+      if(this.player.onchallenge) return;
+      if(!this.player.challenges.includes(index)){
+        this.player.challenges.push(index)
+      }else{
+        this.player.challenges.splice(this.player.challenges.indexOf(index),1)
+      }
+    },
+    buyGenerator(index) {
+      if(this.player.onchallenge && this.player.challenges.includes(6)){
+        if(index>3){
+          return;
+        }
+      }
+      if (this.player.money.greaterThanOrEqualTo(this.player.generatorsCost[index])) {
+        this.player.money = this.player.money.sub(this.player.generatorsCost[index])
+        this.player.generators[index] = this.player.generators[index].add(1)
+        this.player.generatorsBought[index] = this.player.generatorsBought[index].add(1)
+        this.calcgncost()
+      }
+    },
+    buyAccelerator(index) {
+      if(this.player.onchallenge && this.player.challenges.includes(5)) return;
+      if (this.player.money.greaterThanOrEqualTo(this.player.acceleratorsCost[index])) {
+        this.player.money = this.player.money.sub(this.player.acceleratorsCost[index])
+        this.player.accelerators[index] = this.player.accelerators[index].add(1)
+        this.player.acceleratorsBought[index] = this.player.acceleratorsBought[index].add(1)
+        this.calcaccost()
+      }
+    },
+    toggleautobuyer(index){
+      if(index==0)this.genautobuy = !this.genautobuy
+      if(index==1)this.accautobuy = !this.accautobuy
+      if(index==2)this.autolevel = !this.autolevel
+    },
+    buyRewards(index){
+      if(this.player.challengebonuses.includes(index)){
+        this.player.challengebonuses.splice(this.player.challengebonuses.indexOf(index),1)
+        this.player.token += this.challengedata.rewardcost[index]
+      }else{
+        if(this.player.token<this.challengedata.rewardcost[index]){
+          return;
+        }
+        this.player.challengebonuses.push(index)
+        this.player.token -= this.challengedata.rewardcost[index]
+      }
+    },
+    buylevelitems(index){
+      let cost = this.levelshopdata.itemcost[index].pow(this.player.levelitems[index]+1)
+      if(this.player.level.lessThan(cost) || this.player.levelitems[index]>=6){
+        return;
+      }
+      this.player.level = this.player.level.sub(cost);
+      this.player.levelitems[index] = this.player.levelitems[index]+1;
+    },
+    changeMode(index) {
+      this.player.generatorsMode[index] += 1;
+      let maxmode = index
+      if(this.player.onchallenge && this.player.challenges.includes(3)) {
+        maxmode = maxmode/2
+      }
+      if (this.player.generatorsMode[index] > maxmode) {
+        this.player.generatorsMode[index] = 0;
+      }
+    },
+    resetLevel(force,exit) {
+      if(this.player.onchallenge && this.player.challenges.includes(0)){
+        if(this.player.money.lt(new Decimal('1e28'))){
+          alert('現在挑戦1が適用されているため、まだ昇段リセットができません。')
+          return;
+        }
+      }
+      let dividing = 22-2*this.player.rank.add(2).log2()
+      if(dividing<1) dividing = 1
+      let gainlevel = new Decimal(this.player.money.log10()).div(dividing).pow_base(2).round()
+      if(this.activechallengebonuses.includes(12)) gainlevel = gainlevel.mul(new Decimal(2))
+      if (force || confirm('昇段リセットして、段位' + gainlevel + 'を得ますか？')) {
+        if(this.player.onchallenge) {
+          this.player.token = this.player.token + 1
+          this.activechallengebonuses = this.player.challengebonuses;
+          this.player.challengecleared.push(this.calcchallengeid())
+        }
+        let nextlevel = this.player.level.add(exit?new Decimal(0):gainlevel)
+        let nextlevelresettime = this.player.levelresettime.add(this.player.rankresettime.add(1).mul(new Decimal(exit?0:this.activechallengebonuses.includes(8)?2:1)))
+        let nextmaxlevelgained = this.player.maxlevelgained.max(exit?new Decimal(0):gainlevel)
+        let rk = this.player.rank
+        let rkt = this.player.rankresettime
+        let tkn = this.player.token
+        let cls = this.player.challenges
+        let clcleared = this.player.challengecleared
+        let clbonuses = this.player.challengebonuses
+
+        let lvi = this.player.levelitems
+        this.player = initialData()
+        this.player.level = nextlevel
+        this.player.levelresettime = nextlevelresettime
+        this.player.maxlevelgained = nextmaxlevelgained
+        this.player.rank = rk
+        this.player.rankresettime = rkt
+        this.player.token = tkn
+        this.player.challenges = cls
+        this.player.challengecleared = clcleared
+        this.player.challengebonuses = clbonuses
+        this.player.levelitems = lvi
+
+        if(this.activechallengebonuses.includes(0))this.player.money = new Decimal(10001)
+        if(this.activechallengebonuses.includes(1))this.player.accelerators[0] = new Decimal(10)
+
+      }
+    },
+    resetRank(){
+      let gainrank = new Decimal(this.player.money.log10()).div(42).pow_base(2).round()
+      if(confirm('昇階リセットして、階位' + gainrank + 'を得ますか？')){
+
+        let nextrank = this.player.rank.add(gainrank)
+        let nextrankresettime = this.player.rankresettime.add(new Decimal(1))
+
+        let mlg = this.player.maxlevelgained
+        let tkn = this.player.token
+
+        let clcleared = this.player.challengecleared
+        let clbonuses = this.player.challengebonuses
+
+        this.player = initialData()
+
+
+
+        this.player.rank = nextrank
+        this.player.rankresettime = nextrankresettime
+
+        this.player.maxlevelgained = mlg
+        this.player.token = tkn
+
+        this.player.challengecleared = clcleared
+        this.player.challengebonuses = clbonuses
+
+        this.activechallengebonuses = this.player.challengebonuses
+
+        if(this.activechallengebonuses.includes(0))this.player.money = new Decimal(10001)
+        if(this.activechallengebonuses.includes(1))this.player.accelerators[0] = new Decimal(10)
+
+      }
+    },
+    calcchallengeid(){
+      let challengeid = 0;
+      for(let i=0;i<8;i++){
+        challengeid *= 2
+        if(this.player.challenges.includes(i)){
+          challengeid += 1
+        }
+      }
+      return challengeid;
+    },
+    showunclearedchallenges(){
+      if(this.player.challengecleared.length == 255) return;
+      if(this.player.onchallenge) return;
+      let challengeid = this.calcchallengeid();
+
+      do{
+        if(challengeid == 0) {
+          challengeid = 128
+        }else {
+          let idx = this.challengedata.challengeids.indexOf(challengeid) + 1
+          if(idx==255) idx = 0
+          challengeid = this.challengedata.challengeids[idx]
+        }
+      }while(this.player.challengecleared.includes(challengeid));
+
+      let cls = [];
+      for(let i=7;i>=0;i--){
+        if(challengeid%2 == 1)cls.push(i)
+        challengeid = challengeid >>> 1
+
+      }
+      this.player.challenges = cls;
+    },
+    startChallenge(){
+      let challengeid = this.calcchallengeid();
+
+      if(challengeid == 0){
+        alert("挑戦が一つも選択されていません。")
+        return;
+      }
+      if(this.player.challengecleared.includes(challengeid)){
+        alert("すでに達成した挑戦です。")
+        return;
+      }
+
+      if (confirm('挑戦を開始しますか？現在のポイントや発生器、時間加速器は失われます。')) {
+        if(!this.player.challengebonuses.includes(4))this.activechallengebonuses = [];
+        this.resetLevel(true,true);
+        this.player.onchallenge = true;
+        if(this.player.challenges.includes(3)){
+          for(let i=0;i<8;i++){
+            this.player.generatorsMode[i] = i/2
+          }
+        }
+      }
+    },
+    exitChallenge(){
+      if (confirm('挑戦を諦めますか？現在のポイントや発生器、時間加速器を引き継いだまま、通常の状態に入ります。')) {
+        this.player.onchallenge = false;
+        this.activechallengebonuses = this.player.challengebonuses;
+        if(this.player.challenges.includes(1)){
+          for(let i=0;i<8;i++){
+            this.player.generatorsCost[i] = i === 0 ?
+            new Decimal(10).pow(this.player.generatorsBought[0]) :
+            new Decimal(10).pow(this.player.generatorsBought[i].add(i + 1).mul(i + 1))
+          }
+        }
+      }
+    }
+  },
+  mounted() {
+    this.load();
+
+    setTimeout(this.update, this.player.tickspeed);
+    setInterval(this.save, 2000);
+  },
+}).mount('#app');
---
+++ rank.js
@@ -0,0 +1,11 @@
+function Levelshopdata(){
+  this.itemcost = [
+    new Decimal('1e1'),
+    new Decimal('1e2')
+  ],
+  this.itemtext =[
+    "段位リセット1回あたりの効果が弱くなるのが遅くなります。",
+    "新しい時間加速器を購入可能になります。"
+  ]
+
+}
